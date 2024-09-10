ed1e6f9a58dc63d6eba9e36a7538b67b750b2475
Pheletso Mashalan <pheletso.mashalane@standardbank.co.za>
2024-09-10 17:54:55 +0200

    got the dice to roll

--- app/src/main/java/com/example/rolldice/MainActivity.kt
+++ app/src/main/java/com/example/rolldice/MainActivity.kt
@@ -1,15 +1,42 @@
 package com.example.rolldice
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btnDice: Button = findViewById(R.id.roll_dice)
+
+        btnDice.setOnClickListener {
+           rollDice()
+        }
+    }
+
+    fun rollDice(){
+        val viewDice: ImageView = findViewById(R.id.Dice_img)
+        val randomNumber = Random.nextInt(6)+1
+
+        val displayDice = when(randomNumber){
+            1 -> R.drawable.dice_1
+            2-> R.drawable.dice_2
+            3-> R.drawable.dice_3
+            4-> R.drawable.dice_4
+            5-> R.drawable.dice_5
+            6-> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        viewDice.setImageResource(displayDice)
+        Toast.makeText(this, "Rolled a $randomNumber!", Toast.LENGTH_SHORT).show()
     }
 }
